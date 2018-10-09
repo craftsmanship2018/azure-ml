9d5d4533ac3eeee3e2b8142dda6f10f262fe2170
john hanna <john.hanna@computershare.co.uk>
2018-10-09 16:35:18 +0100

    Adding ML Resources

---
+++ Azure-ml Model Creation and Deploy/02. Model Deploy to ACI (Azure Container Instance)/score.py
@@ -0,0 +1,20 @@
+import json
+import numpy as np
+import os
+import pickle
+from sklearn.externals import joblib
+from sklearn.linear_model import LinearRegression
+
+from azureml.core.model import Model
+
+def init():
+    global model
+    # retreive the path to the model file using the model name
+    model_path = Model.get_model_path('car_prediction_model.pkl')
+    model = joblib.load(model_path)
+
+def run(raw_data):
+    data = np.array(json.loads(raw_data)['data'])
+    # make prediction
+    y_hat = model.predict(data)
+    return json.dumps(y_hat.tolist())
